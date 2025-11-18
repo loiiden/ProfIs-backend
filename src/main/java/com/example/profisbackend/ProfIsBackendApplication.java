d37f7c15bb1d630f2d73b1ca1944e5c85663e6b1
Maksim Efremov <loiiden@mac.speedport.ip>
2025-11-18 23:44:59 +0100

    init commit

---
+++ src/main/java/com/example/profisbackend/ProfIsBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.profisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jdbc.JdbcRepositoriesAutoConfiguration;
+
+@SpringBootApplication(exclude = {JdbcRepositoriesAutoConfiguration.class})
+public class ProfIsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfIsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/profisbackend/ProfIsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfIsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
